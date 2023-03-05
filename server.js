24fc1dc5782d4090015ae67925f04af8f463db6f
Jkaur5370 <jaspritk246701@gmail.com>
2023-03-05 13:38:16 +0530

    login form validation and error show

--- server.js
+++ server.js
@@ -9,22 +9,21 @@
 * Course/Section: WEB322 ZBB
 *
 **************************************************************************************/
-
-
-
 import express from 'express';
-import { engine  } from 'express-handlebars';
+import { engine } from 'express-handlebars';
 import { getRentalsByCityAndProvince, getFeaturedRentals } from "./Data Module/rentals-db.js";
 
 const app = express();
 
 
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 app.use(express.static('images'))
 app.use(express.static('css'))
-
+app.use(express.static('js'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.render('home', {
@@ -50,38 +49,46 @@ app.get('/log-in', function (req, res) {
   });
 });
 
+app.post('/log-in', function (req, res) {
+  const email = req?.body?.email;
+  const password = req?.body?.password;
+  var emailError = ""
+  var passwordError = ""
+  if (!email) {
+    emailError = 'Email is required';
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    emailError = 'Invalid email format';
+  }
+  if (!password) {
+    passwordError = 'Password is required';
+  } else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,12}$/.test(password)) {
+    passwordError = 'Password must be between 8 to 12 characters and contain at least one lowercase letter, uppercase letter, number, and symbol';
+  }
+  if (emailError == "" && passwordError == "") {
+    res.send({
+      status: 200,
+      message: "success !!"
+    });
+  } else {
+    res.send({
+      status: 400,
+      message: "error !!",
+      email: emailError,
+      password: passwordError,
+    });
+  }
+});
 
-
-
-
-// *** DO NOT MODIFY THE LINES BELOW ***
-
-// This use() will not allow requests to go beyond it
-// so we place it at the end of the file, after the other routes.
-// This function will catch all other requests that don't match
-// any other route handlers declared before it.
-// This means we can use it as a sort of 'catch all' when no route match is found.
-// We use this function to handle 404 requests to pages that are not found.
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
 });
-
-// This use() will add an error handler function to
-// catch all errors.
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send("Something broke!")
 });
-
-// Define a port to listen to requests on.
 const HTTP_PORT = process.env.PORT || 8080;
-
-// Call this function after the http server starts listening for requests.
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
-
-// Listen on port 8080. The default port for http is 80, https is 443. We use 8080 here
-// because sometimes port 80 is in use by other applications on the machine
 app.listen(HTTP_PORT, onHttpStart);
 
